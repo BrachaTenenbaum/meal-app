12bb7a2e60f37451069d9f28a4f91b545ef512d5
BrachaTenenbaum <brachabrothschild@gmail.com>
2015-12-17 10:10:49 -0700

    first commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 //Create diner objects which represent a single diner.
 var Person = function(name) {
-	// Add dishes to a diner's meal
 	this.dishes = [];
 	this.name = name;
+	this.id = Person.count;
+	Person.count++;
 };
+
+Person.count = 0;
 
 //Dish object which represent a single Dish and its cost.
 var Dish = function(name, cost) {
@@ -11,54 +14,77 @@ var Dish = function(name, cost) {
 	this.cost = cost;
 };
 
-//new Person object and new Dish object:
-// var bracha = new Person('bracha');	
-// var danny = new Person('Danny');
-
-// var people = [bracha, danny];
-
 var people = [];
 
-// var pasta = new Dish('pasta', 5);
-// var pizza = new Dish('pizza', 3);
 
-	 // bracha.dishes.push(pizza, pasta); 
-	 // danny.dishes.push(pasta);
-	 // console.log(bracha.dishes); //--> ["pizza", "pasta"]
+ function creatEventHandlers() {
 
+	$(".diner").submit(function( event ) {
+		event.preventDefault();
 
-// Total up the cost of all of the diner's meals
- // var sum = 
- // var total = 0
- // for (var i = 0; i < bracha.dishes.length; i++) {
- // 		total+= bracha.dishes[i].cost; 
- // };
+		var diner = new Person($('.name').val()); 
+		people.push(diner); 
+		$( "select" ).append('<option value="' + diner.id + '">' + diner.name + '</option>');  //value needs to be in strings in html
+	});
 
- // console.log(total + "total")
+	$(".dish-price").submit(function( event ) {
+		event.preventDefault();
 
-
-
- function display() {
-
-		$(".diner").submit(function( event ) {
-			event.preventDefault();
-
-			var diner = new Person($('.name').val()); 
-			people.push(diner); 
-
-			var food = new Dish($('.dish').val(), $('.cost').val());	
-			diner.dishes.push(food);
-			calculateTotal();
-			$( ".output" ).html("<b>Name:</b>" + diner.name + "<br>" + " <b>Dish:</b>" + food.name + "<br>" + " <b>Price:</b>" + food.cost);
-
-			// ( "<b>Name:</b> " + name +
-			// " <b>dish:</b> " + dish); 
-		});
-
-};
+		var food = new Dish($('.dish').val(), Number($('.cost').val()));
+		var selectedID = $( "select" ).val(); 
+		selectedID = Number(selectedID);
+		// loop through each id until find one that matches the one user clicked on to remove
+		for (var i = 0; i < people.length; i++) {
+			var person = people[i]
+			if (selectedID === person.id) { //or -- people[i].id
+				// then add dish to person here
+				person.dishes.push(food); 
+			}
+		}; 
+		calculateTotal();	 
+		display();
+	});
 
 
-//loop through each person each person
+	$(".remove").click(function() {
+		var selectedID = $( "select" ).val();
+		selectedID = Number(selectedID);
+		$('select option[value="' + selectedID + '"]').remove();
+		//remove person from people array   
+		for (var i = 0; i < people.length; i++) {
+			var person = people[i]
+
+			if (selectedID === person.id) {
+				// then remove person and dish
+				people.splice(i, 1); 
+				break
+			};
+		};
+		// rerender html
+		display();
+	});
+ };
+
+ //loop through and generate html one at a time.
+ function display() { 
+ 	var HTML = '';
+ 	// var personHTML = '';
+ 	// var dishHTML = '';
+
+ 	for (var i = 0; i < people.length; i++) {
+ 		var person = people[i];
+ 		HTML += ('<p> Name: ' + person.name + '</p>');
+ 			for (var j = 0; j < person.dishes.length; j++) {
+ 				var  dish = person.dishes[j];
+ 				HTML += ('<ul><li>' + dish.name + '<p>Price:<p>' + dish.cost + '</li></ul>');
+ 			};
+ 	};
+ 	$(".output").html(HTML);
+ 	
+ };
+
+
+//loop through each person 
 var calculateTotal = function() {
 	var total = 0;
 	for (var i = 0; i < people.length; i++) {
@@ -109,11 +135,8 @@ console.log(totalBill + " Total bill");
 
  // Print a breakdown for what each diner owes	
 
+creatEventHandlers();
 
-
-console.log(people);
-
-display();
 
 
 
